16039c1551be9568d81a2576b0e64fed60075ef9
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2021-01-21 16:51:58 -0500

    added Event listener to index.js file

---
+++ frontend/index.js
@@ -0,0 +1,6 @@
+const BASE_URL = "http://localhost:300/"
+
+document.addEventListener("DOMContentLoaded", () => {
+    createForm();
+    targetForm();
+})
---
+++ frontend/src/calorie.js
@@ -0,0 +1,10 @@
+class Calorie {
+    constructor(id, age, gender, weight, height, bmi) {
+        this.id = id;
+        this.age = age;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
+        this.bmi = bmi 
+    }
+}
