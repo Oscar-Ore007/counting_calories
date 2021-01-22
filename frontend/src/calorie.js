34d5b49e5cb225f16ede0e7f448be6081d8ce7a9
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2021-01-21 21:07:00 -0500

    updated calorie.js file

--- frontend/src/calorie.js
+++ frontend/src/calorie.js
@@ -7,4 +7,40 @@ class Calorie {
         this.height = height;
         this.bmi = bmi 
     }
+
+    viewCalories() {
+        const viewCalories = document.getElementById("calorieValues")
+
+        viewCalories.innerHTML +=
+            
+            `
+            <ul id="calories" class="list" data-set-id="${this.id}">
+                <li>Age: ${this.age}</li>
+                <li>Gender: ${this.gender}</li>
+                <li>Weight: ${this.weight}</li>
+                <li>Height: ${this.height}</li>
+                <li id="bmi" data-set-bmi="${this.bmi}">Calories: ${this.bmi}</li>
+                <input type="button" value="Delete" id="deleteButton">
+
+
+    
+            
+            </ul>
+        
+            `
+        deleteButton.addEventListener("click", deleteCalories)
+
+        document.getElementById("deleteButton").onclick = function () {
+            window.location.reload() 
+        }
+    }
+
+    static calculateCalories(age, gender, weight, height) {
+        if (gender === "Male") {
+            return Math.round((66.47 + (6.3 * weight) + (12.9 * height - (6.8 * age))))
+        }
+        else if (gender === "Female") {
+            return Math.round((665 + (4.3 * weight) + (4.7 * height - (4.7 * age))))
+        }
+    }
 }
